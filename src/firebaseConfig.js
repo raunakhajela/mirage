ef57d2ec8b56941b1861061b4e6888d7d8ce3f22
tbhaxor <tbhaxor@gmail.com>
2018-10-08 12:10:04 +0530

    deleted

--- src/firebaseConfig.js
+++
@@ -1,8 +0,0 @@
-export default {
-    apiKey: "ENTER VALUE HERE",
-    authDomain: "ENTER VALUE HERE",
-    databaseURL: "ENTER VALUE HERE",
-    projectId: "ENTER VALUE HERE",
-    storageBucket: "ENTER VALUE HERE",
-    messagingSenderId: "ENTER VALUE HERE"
-};
